c628435d677bbae329ac1297c2f756036dd295f5
Collin Stewart <cstewa91@gmail.com>
2018-11-28 15:38:25 -0800

    completed project setup

---
+++ src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default props => (
+   <div>
+      <h1 className='center'>Stuff about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod id, ullam non repellat adipisci corporis similique quidem saepe nesciunt eum molestiae velit perspiciatis voluptatibus libero nulla aliquam dolores? Ea, corporis!</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, nisi?
+         .
+      </p>
+   </div>
+);
